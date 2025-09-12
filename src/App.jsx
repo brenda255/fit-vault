0beb5a833ca68c858eaa67f145ad53c0ed12eecc
brenda <115198624+brenda255@users.noreply.github.com>
2025-09-12 14:15:38 -0600

    login/logout shows different nav items

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,21 @@ import Exercises from "./pages/Exercises"
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
 import Recap from "./pages/Recap"
+import { useState } from "react"
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
   return (
     <Router>
-      <Nav />
+      <Nav isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/AddWorkout' element={<AddWorkout/>}/>
-        <Route path='/Exercises' element={<Exercises/>}/>
-        <Route path='/Login' element={<Login/>}/>
-        <Route path='/Profile' element={<Profile/>}/>
-        <Route path='/Recap' element={<Recap/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/AddWorkout' element={<AddWorkout />} />
+        <Route path='/Exercises' element={<Exercises />} />
+        <Route path='/Profile' element={<Profile />} />
+        <Route path='/Recap' element={<Recap />} />
+        <Route path='/Login' element={<Login />} />
       </Routes>
     </Router>
   )
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,27 @@
 import { Link } from "react-router-dom";
 
-function Nav () {
+function Nav({ isLoggedIn, setIsLoggedIn }) {
     return (
         <nav>
-            <Link to= "/">Home</Link>
-            <Link to ="/AddWorkout">Add Workout</Link>
-            <Link to ="/Exercises">Exercises</Link>
-            <Link to ="/Login">Login</Link>
-            <Link to ="/Profile">Profile</Link>
-            <Link to ="/Recap">Recap</Link>
+            <Link to="/">Home</Link>
+            {isLoggedIn ? (
+                //show these links when logged in
+                <>
+                    <Link to="/AddWorkout">Add Workout</Link>
+                    <Link to="/Exercises">Exercises</Link>
+                    <Link to="/Profile">Profile</Link>
+                    <Link to="/Recap">Recap</Link>
+                    <button onClick={() => setIsLoggedIn(false)}>Logout</button>
+                </>
+            ) : (
+                //these show when NOT logged in
+                <>
+                    <Link to="/Login">Login</Link>
+                    <button onClick={() => setIsLoggedIn(true)}>Login</button>
+
+                </>
+            )}
+
         </nav>
     )
 }
